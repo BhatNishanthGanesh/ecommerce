3bedcf623fa0c4500e09df4e8b37a95bb9082d41
Chethan Pai Maroor <mcpai217@gmail.com>
2023-10-17 10:37:59 +0530

    About us

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Home from "./pages/Home";
-
+import About from "./pages/About";
 import Footer from "./components/footer";
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/footer" element={<Footer/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
     </Routes>
      
     </BrowserRouter>
---
+++ ecommerce/src/pages/About.js
@@ -0,0 +1,13 @@
+// src/components/AboutUs.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the Ecomerce site created by bhat nishanth ganesh.</p>
+    </div>
+  );
+};
+
+export default About;
